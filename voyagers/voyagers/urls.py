9aae232155d62b2d2022e15bd0e25cffefcc0fc2
trnguyen1510 <trnguyen1510@gmail.com>
2020-10-25 20:45:07 -0700

    added new app

--- voyagers/voyagers/urls.py
+++ voyagers/voyagers/urls.py
@@ -23,7 +23,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path('', include('home.urls'))
+        path('', include('home.urls')),
+        ##path('dashboard/', include('dashboard.urls'))
         
         
 
