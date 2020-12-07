398f42b5d85efe087e86ac57e927705b975ef1b5
trnguyen1510 <trnguyen1510@gmail.com>
2020-12-06 20:31:28 -0800

    fixed the survey

--- voyagers/survey/models.py
+++ voyagers/survey/models.py
@@ -11,15 +11,15 @@ class Survey(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     country = models.CharField(max_length=100, blank=True, default='')
-    city = models.CharField(blank=True,max_length=100, default='')
-    tour = models.CharField(blank=True,max_length=100, default='')
+    city = models.CharField(blank=True, max_length=100, default='')
+    tour = models.CharField(blank=True, max_length=100, default='')
     departure = models.CharField(blank=True, max_length=100, default='')
     futureCompanionDescription = models.TextField(blank=True, default='')
 
     def __str__(self):
         return self.user.username
 
-"""
+
 @receiver(post_save, sender=User)
 def create_surveyAnswer(sender, instance, created, **kwargs):
     if created:
@@ -29,7 +29,6 @@ def create_surveyAnswer(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_surveyAnswer(sender, instance, **kwargs):
     instance.survey.save()
-"""
 
 # class Answer_Option(models.Model):
 #     survey = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -37,4 +36,3 @@ def save_surveyAnswer(sender, instance, **kwargs):
 #
 #     def __unicode__(self):
 #         return self.text
-
